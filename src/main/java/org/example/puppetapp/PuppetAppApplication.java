8362a1d250be811def9b9b123b09a00d0448ba52
Ray <rlill23@wgu.edu>
2026-06-07 22:45:21 -0400

    Initial commit.

---
+++ src/main/java/org/example/puppetapp/PuppetAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.puppetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppetAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/puppetapp/PuppetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.puppetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
