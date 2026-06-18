95dfce41424a629ca5562db933e7cdb4d0030cee
Ray <raymond.lilly4@gmail.com>
2026-06-18 13:10:29 -0400

    Worked with actuators

---
+++ README.md
@@ -0,0 +1,2 @@
+Puppet app for the Followers of God Puppet Team to help with coordinating positions and making every member
+an expert of their position.
---
+++ src/main/java/org/example/puppetapp/rest/HomePageRestController.java
@@ -0,0 +1,13 @@
+package org.example.puppetapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomePageRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
