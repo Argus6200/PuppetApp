b916c05e5d4d55d594b3992b4c3cf5b520fd02d8
Ray <raymond.lilly4@gmail.com>
2026-06-18 14:47:03 -0400

    Configured some application properties by adding team.name

--- src/main/java/org/example/puppetapp/rest/HomePageRestController.java
+++ src/main/java/org/example/puppetapp/rest/HomePageRestController.java
@@ -1,13 +1,20 @@
 package org.example.puppetapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomePageRestController {
 
+    @Value("${team.name}")
+    private String teamName;
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
     }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){return "Team Name: " + teamName;}
 }
