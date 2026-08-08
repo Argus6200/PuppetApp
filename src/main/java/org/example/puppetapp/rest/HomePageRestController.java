6d2df71b0bc8729c8fa338388f2d02d1fe4536a2
Ray <raymond.lilly4@gmail.com>
2026-08-08 12:33:20 -0400

    Worked with REST Controllers

--- src/main/java/org/example/puppetapp/rest/HomePageRestController.java
+++ src/main/java/org/example/puppetapp/rest/HomePageRestController.java
@@ -2,15 +2,17 @@ package org.example.puppetapp.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class HomePageRestController {
 
     @Value("${team.name}")
     private String teamName;
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String sayHello(){
         return "Index.html";
     }
